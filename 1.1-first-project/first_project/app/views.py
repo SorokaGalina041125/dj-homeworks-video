90a40df00351c027d10340e744b090c201477f18
Soroka Galina <galina.me@icloud.com>
2026-04-20 11:15:59 +0400

    task 1.1 Introduction to Django: implement home page, current time and workdir views

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from django.urls import reverse 
+import datetime
+import os
 
 
 def home_view(request):
@@ -8,8 +11,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,8 +26,9 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    current_time =  datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    msg = f'Текущее время: {formatted_time}'
     return HttpResponse(msg)
 
 
@@ -32,4 +36,11 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
+    files = os.listdir('.') 
+    result = "<h1>Содержимое рабочей директории:</h1><ul>"
+    for file in files:
+        result += f"<li>{file}</li>"
+    result += "</ul>"
+    
+    return HttpResponse(result)
     raise NotImplemented
